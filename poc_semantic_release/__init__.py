a4fb0a0909aa87ea37967606e3156eb87cd32592
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-02-17 17:02:55 -0400

    fix: Fix release versioning in files in place

--- poc_semantic_release/__init__.py
+++ poc_semantic_release/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for PoC Semantic Release."""
 
 __author__ = """Ivan Ogasawara"""
-__email__ = 'ivan.ogasawara@gmail.com'
-__version__ = '1.1.9'
+__email__ = "ivan.ogasawara@gmail.com"
+__version__ = "1.1.9
